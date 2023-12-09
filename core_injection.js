8fdc14b3d097b6eb5d4abd9eff11102843793dbf
ycat36 <74068072+IsaacHatton@users.noreply.github.com>
2023-12-09 17:51:48 +0000

    use chrome.runtime.getURL for svg

--- core_injection.js
+++ core_injection.js
@@ -22,8 +22,6 @@ async function injectOtherScripts()
 	injectScript('lib/pako.js');
 	await injectScript('core/multi_device.js');
 	injectScript('core/interception.js');
-	
-	injectScript("core/status_download.js")
 
 	setTimeout(
 		function() {injectScript('lib/moduleraid.js');},
