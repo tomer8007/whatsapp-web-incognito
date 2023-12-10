6ebe97d15ad36c173e461bf494caf73e5a6d751f
ycat36 <74068072+IsaacHatton@users.noreply.github.com>
2023-12-10 17:52:29 +0000

    change MutationObserver

--- core/status_download.js
+++ core/status_download.js
@@ -1,73 +1,113 @@
-// 
+//
 // When a photo/video status is being viewed, create a button to download it that appears, and then handle downloading it
 //
 
-function checkIfStatusModeActive(){
-    // check if there are any div elements with innerText "Type a reply…" in the document body
-    var divs = document.getElementsByTagName("div")
-    for(var i = 0; i < divs.length; i++){
-        if(divs[i].innerText == 'Type a reply…'){
-            return true
-        }
+function destroyOldButton(src) {
+    var buttons = document.getElementsByClassName("download-button-incognito");
+    // loop through buttons
+    for (var i = 0; i < buttons.length; i++) {
+      // if the button's src matches the src of the img
+      console.log("src" + buttons[i].href);
+      console.log("buttons[i]" + buttons[i]);
+      if (buttons[i].href == src) {
+        // remove the button
+        buttons[i].remove();
+      }
     }
-    return false
-}
-
-function destroyOldButtons(){
-    var buttons = document.getElementsByClassName("download-button-incognito")
-    while(buttons.length > 0){
-        buttons[0].parentNode.removeChild(buttons[0])
-    }
-}
-
-function createDownloadButton(src){
+  }
+  
+  function createDownloadButton(src) {
     var a = document.createElement("a");
-    a.innerHTML = "<img src='" + chrome.runtime.getURL("images/download.svg") + "' >"
+    a.innerHTML =
+      "<img src='" + chrome.runtime.getURL("images/download.svg") + "' >";
     a.className = "download-button-incognito";
     a.href = src;
     a.download = "status";
     document.body.appendChild(a);
-}
-
-function checkIfButtonNeeded(){
-    var needed = false
-    if(checkIfStatusModeActive()){
-        try{
-            if(document.getElementsByTagName("video")[0].src.startsWith("blob")){
-                needed = true
-                var src = document.getElementsByTagName("video")[0].src
+  }
+  
+  // create a mutation observer
+  var observer = new MutationObserver(function (mutations) {
+    for (var i = 0; i < mutations.length; i++) {
+      var addedNodes = mutations[i].addedNodes;
+      var removedNodes = mutations[i].removedNodes;
+      // for each added node
+      if (addedNodes && addedNodes.length > 0) {
+        for (var j = 0; j < addedNodes.length; j++) {
+          isStatusChange = false;
+          // check if this node change is a status view by looping through all divs and checking for one with the innerText containity "Type a reply"
+          var divs = addedNodes[j].getElementsByTagName("div");
+          // if there are any
+          if (divs.length > 0) {
+            // for each div
+            for (var k = 0; k < divs.length; k++) {
+              // if the div contains the text "Type a reply"
+              if (divs[k].innerText.includes("Type a reply")) {
+                isStatusChange = true;
+              }
             }
-        }catch(e){
-            // attempt to find a status photo on the page
-            try {
-                let images = document.getElementsByTagName("img");
-                for (let i = 0; i < images.length; i++) {
-                    if (images[i].src.startsWith("blob")) {
-                        needed = true
-                        var src = images[i].src
-                        break
-                    }
+          }
+          if (isStatusChange) {
+            // check if this node contains any nodes that are img/video (at any depth)
+            var imgs = addedNodes[j].getElementsByTagName("img");
+            var videos = addedNodes[j].getElementsByTagName("video");
+            if (imgs.length > 0) {
+              // for each img
+              for (var k = 0; k < imgs.length; k++) {
+                // if the img src starts with blob
+                if (imgs[k].src.startsWith("blob")) {
+                  console.log("THIS IS IT");
+                  console.log(addedNodes[j]);
+                  // create a button
+                  createDownloadButton(imgs[k].src);
                 }
-            } catch {
-                needed = false;
+              }
+            } else if (videos.length > 0) {
+              for (var k = 0; k < videos.length; k++) {
+                // if the img src starts with blob
+                if (videos[k].src.startsWith("blob")) {
+                  // create a button
+                  createDownloadButton(videos[k].src);
+                }
+              }
             }
+          }
         }
-    }else{
-        needed = false
+      }
+      // for each removed node
+      if (removedNodes && removedNodes.length > 0) {
+        for (var j = 0; j < removedNodes.length; j++) {
+          // check if this removed node contains any nodes that are img (at any depth)
+          var imgs = removedNodes[j].getElementsByTagName("img");
+          if (imgs.length > 0) {
+            // for each img
+            for (var k = 0; k < imgs.length; k++) {
+              // if the img src starts with blob
+              if (imgs[k].src.startsWith("blob")) {
+                // remove the button
+                destroyOldButton(imgs[k].src);
+              }
+            }
+          }
+          // check if this removed node contains any nodes that are video (at any depth)
+          var videos = removedNodes[j].getElementsByTagName("video");
+          if (videos.length > 0) {
+            // Loop through each video
+            for (var k = 0; k < videos.length; k++) {
+              // Check if the video src starts with blob
+              if (videos[k].src.startsWith("blob")) {
+                // Remove the button
+                destroyOldButton(imgs[k].src);
+              }
+            }
+          }
+        }
+      }
     }
-    destroyOldButtons()
-    if(needed){
-        createDownloadButton(src)
-    }
-}
-
-// create a mutation observer
-var observer = new MutationObserver(function(mutations) {
-    checkIfButtonNeeded()
-});
-
-// start observing
-observer.observe(document.getElementById("app"), {
+  });
+  
+  // start observing
+  observer.observe(document.getElementById("app"), {
     childList: true,
-    subtree: true
-});
+    subtree: true,
+  });
