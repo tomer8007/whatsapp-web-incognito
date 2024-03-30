6dccce049ad53fb72e69804b366a8b42602a4ed2
Tomer <11458759+tomer8007@users.noreply.github.com>
2024-03-30 17:11:22 +0300

    WIP: Added backward-compatible UI class names for WA version 2.24

--- core/ui_class_names.js
+++ core/ui_class_names.js
@@ -5,27 +5,58 @@ var UIClassNames = {};
 
 (function() {
 
-UIClassNames.MENU_ITEM_CLASS = "_ajv7";
-UIClassNames.MENU_ITEM_HIGHLIGHTED_CLASS = "_ajv9";
 
-UIClassNames.OUTER_DROPDOWN_CLASS = "_ak4w"; // outside class of dropdown with "mark as unread"
-UIClassNames.DROPDOWN_CLASS = "_ak5b";
-UIClassNames.DROPDOWN_ENTRY_CLASS = "_aj-r _aj-q _aj-_";
+if (document.getElementById("whatsapp-web"))
+{
+    // Seems to be WhatsApp version 2.3000+
 
-UIClassNames.UNREAD_COUNTER_CLASS = "x18ba5f9"; // the class that sets background-color to var(--unread-marker-background)
+    UIClassNames.MENU_ITEM_CLASS = "_ajv7";
+    UIClassNames.MENU_ITEM_HIGHLIGHTED_CLASS = "_ajv9";
+    
+    UIClassNames.OUTER_DROPDOWN_CLASS = "_ak4w"; // outside class of dropdown with "mark as unread"
+    UIClassNames.DROPDOWN_CLASS = "_ak5b";
+    UIClassNames.DROPDOWN_ENTRY_CLASS = "_aj-r _aj-q _aj-_";
+    
+    UIClassNames.UNREAD_COUNTER_CLASS = "x18ba5f9"; // the class that sets background-color to var(--unread-marker-background)
+    
+    UIClassNames.CHAT_PANEL_CLASS = "_amm9"; // the outermost chat panel class
+    UIClassNames.INNER_CHAT_PANEL_CLASS = "_ajyl"; // the innermost chat panel class ('conversation-panel-messages')
+    
+    UIClassNames.CHAT_ENTRY_CLASS = "x10l6tqk "; // the chat entry in the chats list
+    UIClassNames.UNREAD_MARKER_CLASS = "_agtk"; // the "X unread messages" warning inside the chat panel; the class inside the top-level item
+    
+    UIClassNames.CHAT_MESSAGE = "messag-in";
+    UIClassNames.CHAT_MESSAGE_INNER_TEXT_DIV = "x1lliihq xh8yej3";
+    
+    
+    UIClassNames.TEXT_WRAP_POSITION_CLASS = "_21Ahp";
+    // TODO
+    UIClassNames.DELETED_MESSAGE_SPAN = "_20bHr";
+    UIClassNames.STICKER_MESSAGE_TAG = "_3mPXD";
+}
+else
+{
+    // Seems to be WhatsApp version 2.24
 
-UIClassNames.CHAT_PANEL_CLASS = "_amm9"; // the outermost chat panel class
-UIClassNames.INNER_CHAT_PANEL_CLASS = "_ajyl"; // the innermost chat panel class ('conversation-panel-messages')
+    UIClassNames.MENU_ITEM_CLASS = "_3OtEr";
+    UIClassNames.MENU_ITEM_HIGHLIGHTED_CLASS = "_2Qn52";
 
-UIClassNames.CHAT_ENTRY_CLASS = "x10l6tqk "; // the chat entry in the chats list
-UIClassNames.UNREAD_MARKER_CLASS = "_agtk"; // the "X unread messages" warning inside the chat panel; the class inside the top-level item
+    UIClassNames.OUTER_DROPDOWN_CLASS = "_2sDI2"; // outside class of dropdown with "mark as unread"
+    UIClassNames.DROPDOWN_CLASS = "_3bcLp";
+    UIClassNames.DROPDOWN_ENTRY_CLASS = "Iaqxu FCS6Q jScby";
 
-UIClassNames.CHAT_MESSAGE = "messag-in";
-UIClassNames.CHAT_MESSAGE_INNER_TEXT_DIV = "x1lliihq xh8yej3";
+    UIClassNames.UNREAD_COUNTER_CLASS = "ovhn1urg"; // the class that sets background-color to var(--unread-marker-background)
+
+    UIClassNames.CHAT_PANEL_CLASS = "_2gzeB"; // the outermost chat panel class
+    UIClassNames.INNER_CHAT_PANEL_CLASS = "_5kRIK"; // the innermost chat panel class ('conversation-panel-messages')
+
+    UIClassNames.CHAT_ENTRY_CLASS = "rx9719la"; // the chat entry in the chats list
+    UIClassNames.UNREAD_MARKER_CLASS = "_2jRew"; // the "X unread messages" warning inside the chat panel; the class inside the top-level item
+
+    UIClassNames.CHAT_MESSAGE = "messag-in";
+    UIClassNames.CHAT_MESSAGE_INNER_TEXT_DIV = "_21Ahp c4mcV";
 
 
-UIClassNames.TEXT_WRAP_POSITION_CLASS = "_21Ahp";
-// TODO
-UIClassNames.DELETED_MESSAGE_SPAN = "_20bHr";
-UIClassNames.STICKER_MESSAGE_TAG = "_3mPXD";
+    UIClassNames.TEXT_WRAP_POSITION_CLASS = "_21Ahp";
+}
 })();
